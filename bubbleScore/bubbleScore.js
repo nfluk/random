a43c58ead3c88e3e9f819ca43edf551208927082
deroj <nickylukic@gmail.com>
2021-08-12 13:49:44 +0200

    changed "scores[0]" output to "scores[ i ]"

--- bubbleScore/bubbleScore.js
+++ bubbleScore/bubbleScore.js
@@ -3,7 +3,7 @@ var scores = [33, 57, 223];
 var i = 0;
 
 while (i < scores.length) {
-  output = "Bubble solution #" + i + " score: " + scores[0];
+  output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
   i = i + 1;
 }
