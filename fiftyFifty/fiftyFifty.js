a8e700ed5322ef25c483f97e47d3450857107f84
deroj <nickylukic@gmail.com>
2021-09-28 15:43:48 +0200

    added if flip ...

---
+++ fiftyFifty/fiftyFifty.js
@@ -0,0 +1,10 @@
+document.getElementById("flipper").onclick = function () {
+  let flip = Math.round(Math.random());
+  if (flip === 1) {
+    console.log("Heads");
+    console.log(flip);
+  } else {
+    console.log("Tails");
+    console.log(flip);
+  }
+};
