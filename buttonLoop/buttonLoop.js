649510c1c6156c58dc4705cb289e6545f5adf549
deroj <nickylukic@gmail.com>
2021-09-30 16:21:43 +0200

    defined looper function

--- buttonLoop/buttonLoop.js
+++ buttonLoop/buttonLoop.js
@@ -1,4 +1,13 @@
+function looper() {
+  var loopCount = 5;
+
+  while (loopCount > 0) {
+    console.log("LoopCount: " + loopCount);
+    loopCount = loopCount - 1;
+  }
+}
+
 document.getElementById("button").onclick = function () {
-  console.log("Test");
+  looper();
   this.style.color = "red";
 };
