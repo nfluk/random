04d85ee42fed86851db89741d93ff0a5e0d61805
deroj <nickylukic@gmail.com>
2021-10-01 13:18:55 +0200

    added a for loop and a return

--- repeatedFlipper/repeatedFlipper.js
+++ repeatedFlipper/repeatedFlipper.js
@@ -1,8 +1,18 @@
-for (let i = 0; i < 5; i++) {
-  coinFlip = Math.round(Math.random());
-  if (coinFlip === 1) {
-    console.log("#" + (i + 1) + " Heads");
-  } else {
-    console.log("#" + (i + 1) + " Tails");
+function flip(number) {
+  var countHeads = 0;
+
+  for (let i = 0; i < number; i++) {
+    var coinFlip = Math.round(Math.random());
+
+    if (coinFlip === 1) {
+      console.log(i + 1 + ". heads");
+      countHeads = countHeads + 1;
+    } else {
+      console.log(i + 1 + ". tails");
+    }
   }
+
+  return countHeads / number;
 }
+
+flip(12);
