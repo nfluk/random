3296a054263929374dc0ed292fcfa5fe35e98002
deroj <nickylukic@gmail.com>
2021-10-07 11:25:58 +0200

    added while loop

--- fizzBuzz/start.js
+++ fizzBuzz/start.js
@@ -1,7 +1,13 @@
+var num = prompt();
+
 function fizzBuzz(num) {
-  if (num % 3 == 0) {
-    console.log("Fizz");
-  } else if (num % 5 == 0) {
-    console.log("Buzz");
-  } else console.log(num);
+  var start = 1;
+  while (start < num) {
+    if (start % 3 == 0) {
+      console.log("Fizz");
+    } else if (start % 5 == 0) {
+      console.log("Buzz");
+    } else console.log(start);
+    start++;
+  }
 }
