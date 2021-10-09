05c9186244a8c806fbc11f93979e84cbad303778
deroj <nickylukic@gmail.com>
2021-10-09 14:59:22 +0200

    refactored "document...."

--- fizzBuzz/start.js
+++ fizzBuzz/start.js
@@ -1,17 +1,18 @@
 function fizzBuzz(num) {
-  var start = 1;
-  while (start < num) {
-    if (start % 3 == 0) {
-      console.log("Fizz");
-    } else if (start % 5 == 0) {
-      console.log("Buzz");
-    } else console.log(start);
-    start++;
-  }
+  if (num <= 100 && num >= 0) {
+    var start = 1;
+    while (start < num) {
+      if (start % 3 == 0) {
+        console.log("Fizz");
+      } else if (start % 5 == 0) {
+        console.log("Buzz");
+      } else console.log(start);
+      start++;
+    }
+  } else alert("Your number is not within the range 1-100.");
 }
 
 document.getElementById("StartButton").onclick = function () {
-  prompt("Enter a number:");
-  var num = prompt();
+  var num = prompt("Enter a number within the range 1-100:");
   fizzBuzz(num);
 };
