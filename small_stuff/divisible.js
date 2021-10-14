597106c5f3c2e6bf519c0248e6ccc6382d3f5de4
deroj <nickylukic@gmail.com>
2021-10-14 14:10:47 +0200

    added isDivisibleBy function

--- small_stuff/divisible.js
+++ small_stuff/divisible.js
@@ -1,3 +1,11 @@
+// checks if number is divisible by 7
+
 function isDivisibleBy7(num) {
   return num % 7 === 0;
+}
+
+// checks if first number is divisible by second number
+
+function isDivisibleBy(num1, num2) {
+  return num1 % num2 === 0;
 }
