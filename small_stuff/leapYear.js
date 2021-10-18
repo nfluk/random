9a9396d5796e985c84a78f5b1a11bdb0f87f3da2
deroj <nickylukic@gmail.com>
2021-10-18 16:28:16 +0200

    redefined the entire function (was faulty)

--- small_stuff/leapYear.js
+++ small_stuff/leapYear.js
@@ -3,14 +3,15 @@
 function isLeapYear(year) {
   var result;
 
-  if (year % 4 === 0 && year % 100 === 0) {
-    if (year % 400 !== 0) {
-      result = false;
-    } else {
-      result = true;
-    }
-  } else {
+  if (year % 4 !== 0) {
     result = false;
+  } else if (year % 100 !== 0) {
+    result = true;
+  } else if (year % 400 !== 0) {
+    result = false;
+  } else {
+    result = true;
   }
+
   return result;
 }
