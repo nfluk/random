4f114bc99211d2e6500c438e3892a0555f0335e3
deroj <nickylukic@gmail.com>
2021-10-20 19:03:13 +0200

    first commit

---
+++ small_stuff/sumFirstOneHundred.js
@@ -0,0 +1,12 @@
+// a function that adds up all the numbers between 1 and 100
+
+function firstOneHundred() {
+  var sum = 0;
+  var num;
+
+  for (num = 0; num <= 100; num++) {
+    sum = sum + num;
+  }
+
+  return sum;
+}
