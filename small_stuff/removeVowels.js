95b04050f7c8d92043302fdfd9fc17028a6d23e8
deroj <nickylukic@gmail.com>
2021-10-21 17:14:56 +0200

    added if statement to only accept string as input

--- small_stuff/removeVowels.js
+++ small_stuff/removeVowels.js
@@ -1,6 +1,10 @@
 // function that replaces vowels in a string with a star (*)
 
 function removeVowel(message) {
+  if (typeof message !== "string") {
+    throw "the input to removeVowel must be a string!";
+  }
+
   for (i = 0; i < message.length; i++) {
     if (
       message.charAt(i) === "a" ||
