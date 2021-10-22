287090035d17a0a56400d8ec3ac87917fbf0b1b0
deroj <nickylukic@gmail.com>
2021-10-22 14:38:45 +0200

    added throw if input is not a string

--- small_stuff/firstLowerCase.js
+++ small_stuff/firstLowerCase.js
@@ -1,6 +1,10 @@
 // returns the first lower case letter in a string
 
 function firstLowerCase(tweet) {
+  if (typeof tweet !== "string") {
+    throw "the input to firstLowerCase needs to be a string!";
+  }
+
   for (i = 0; i < tweet.length; i++) {
     if (
       tweet.charAt(i) === tweet.charAt(i).toLowerCase() &&
