9c5064b7616df9c9ea0d55391734dfe6be86a72c
deroj <nickylukic@gmail.com>
2021-10-22 11:06:48 +0200

    first commit

---
+++ small_stuff/pigLatinLoop.js
@@ -0,0 +1,38 @@
+// pigLatin using a while loop
+
+function pigLatin(word) {
+  if (typeof word !== "string") {
+    throw "The input for pigLatin needs to be a string!!!";
+  }
+
+  function vowel(letter) {
+    if (
+      letter !== "a" ||
+      letter !== "e" ||
+      letter !== "i" ||
+      letter !== "o" ||
+      letter !== "u"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  var result;
+  var secondPart = "";
+  var i = 0;
+
+  while (word.charAt(i) !== vowel && i < word.length) {
+    secondPart = secondPart + word.charAt(i);
+    i++;
+  }
+
+  var start = word.length - secondPart.length;
+
+  var firstPart = word.substr(start, word.length);
+
+  result = firstPart + secondPart + "ay";
+
+  return result;
+}
