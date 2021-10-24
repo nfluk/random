2092fba05cd8194fc238da56a6482f2bb7fd1b21
deroj <nickylukic@gmail.com>
2021-10-24 15:51:48 +0200

    corrected error of curly brackets in if statement

--- small_stuff/sumAToB.js
+++ small_stuff/sumAToB.js
@@ -1,24 +1,22 @@
 // sums the whole numbers from a to b and returns the sum
 
-function sumAToB (a, b) {
-    if (typeof a && typeof b !== "number") {
-    throw "You need to input two numbers!"
-    }
+function sumAToB(a, b) {
+  if (typeof a && typeof b !== "number") {
+    throw "You need to input two numbers!";
+  }
 
-    var result = 0;
+  var result = 0;
 
-    if (a < b) {
-    
+  if (a < b) {
     for (i = a; i <= b; i++) {
-    result += i;
-} else if (a > b) {
+      result += i;
+    }
+  } else if (a > b) {
     for (i = b; i <= b; i++) {
-    result += i;
-} else {
-    result = "Something went wrong. Please try again."
-}
-}
-}
-
-    return result;
+      result += i;
+    }
+  } else {
+    result = "Something went wrong. Please try again.";
+  }
+  return result;
 }
