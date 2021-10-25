ea6aa015fbbb4beafdeb3144103ddcfe747d8a73
deroj <nickylukic@gmail.com>
2021-10-25 18:45:53 +0200

    first commit

---
+++ small_stuff/reverseString.js
@@ -0,0 +1,15 @@
+// takes a string and returns the string in reverse as output
+
+function reverseString(message) {
+  if (typeof message !== "string") {
+    throw "You need to enter a string!!!";
+  }
+
+  var result = "";
+
+  for (i = message.length - 1; i >= 0; i--) {
+    result = result + message.charAt(i);
+  }
+
+  return result;
+}
