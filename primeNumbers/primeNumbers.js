e3cf59723e5dcfa4394aa85097ee2f71177d7e74
deroj <nickylukic@gmail.com>
2021-11-01 16:45:46 +0100

    commented out buggy code & added comment to return

--- primeNumbers/primeNumbers.js
+++ primeNumbers/primeNumbers.js
@@ -1,7 +1,10 @@
 // var num = parseInt(document.getElementById("prime").value);
 
-function isPrime() {
-  var num = parseInt(document.getElementById("prime").value);
+/**
+ * don't yet know why tis isn't working so will return when I know more to fix it (if I remember)
+ */
+// function isPrime() {
+//   var num = parseInt(document.getElementById("prime").value);
 
-  alert(num);
-}
+//   alert(num);
+// }
