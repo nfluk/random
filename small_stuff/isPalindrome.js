2cad07ded816bd33dcd3440c11d379ccf417419f
deroj <nickylukic@gmail.com>
2021-11-03 00:53:39 +0100

    defined function

--- small_stuff/isPalindrome.js
+++ small_stuff/isPalindrome.js
@@ -1,1 +1,17 @@
 // checks to see if a string is a palindrome or not
+
+function isPalindrome(string) {
+  if (string !== "string") {
+    throw "You need to enter a string!";
+  }
+
+  var result = "";
+
+  if (typeof string === reverseString) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
+}
