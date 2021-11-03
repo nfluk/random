d152c45f985115d727bdbfa6ff76efbd5b8bb67d
deroj <nickylukic@gmail.com>
2021-11-03 01:08:24 +0100

    imported reverseString

--- small_stuff/isPalindrome.js
+++ small_stuff/isPalindrome.js
@@ -1,4 +1,24 @@
-// checks to see if a string is a palindrome or not
+// checks to see if a string is a palindrome or not - takes whitespace into account
+
+import reverseString from "./reverseString";
+
+function isPalindrome(string) {
+  if (string !== "string") {
+    throw "You need to enter a string!";
+  }
+
+  var result = "";
+
+  if (typeof string === reverseString(string)) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
+}
+
+// disregards whitespace
 
 function isPalindrome(string) {
   if (string !== "string") {
