ac6fb0fc1f5306e9147f9dc46564bc58156d8424
deroj <nickylukic@gmail.com>
2021-11-04 01:27:28 +0100

    first commit

---
+++ small_stuff/greetings.js
@@ -0,0 +1,7 @@
+// defining and console logging an array of greetings
+
+var greetings = ["hello", "hi", "aloha", "cao", "hej", "bonjour"];
+
+for (i = 0; i < greetings.length; i++) {
+  console.log(greetings[i]);
+}
