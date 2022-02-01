aca500b215a512af0043e8069498d24e43286c1c
deroj <nickylukic@gmail.com>
2022-02-01 14:38:47 +0100

    fixed typo

--- hf_freeman/scoops.js
+++ hf_freeman/scoops.js
@@ -2,7 +2,7 @@ var scoops = 5;
 
 while (scoops > 0) {
   document.write("Another scoop, please! <br>");
-  scoops = -1;
+  scoops -= 1;
 }
 
 document.write("Life without ice cream isn't the same.");
