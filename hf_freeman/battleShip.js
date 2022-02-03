ab0cd4992d9c534bfd21d41ee5e83132b70a85d6
deroj <nickylukic@gmail.com>
2022-02-03 12:00:11 +0100

    refined game flow comment

--- hf_freeman/battleShip.js
+++ hf_freeman/battleShip.js
@@ -6,8 +6,7 @@ General flow of game:
 2. game places a ship at a random location on grid
 3. game play begins. Repeat the following until ship sunk:
     3.1. prompt user to guess a number
-    3.2. check if hit or miss
-        3.2.1. if entire ship sunk end game
+    3.2. check if hit, miss, or sink
 4. game finished & user gets rating based on number of guesses
 
 */
