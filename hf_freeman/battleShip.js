52dc6a103e94d0128c580e632df665efb506478e
deroj <nickylukic@gmail.com>
2022-02-03 11:57:12 +0100

    first commit

---
+++ hf_freeman/battleShip.js
@@ -0,0 +1,12 @@
+/* 
+
+High-level game setup:
+1. user starts game
+2. game places a ship at a random location on grid
+3. game play begins. Repeat the following until ship sunk:
+    3.1. prompt user to guess a number
+    3.2. check if hit or miss
+    3.3. if entire ship sunk end game
+4. game finished & user gets rating based on number of guesses
+
+*/
