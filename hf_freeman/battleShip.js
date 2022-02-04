7cbf4ae7b68185a62d485b07f00792239cbd2672
deroj <nickylukic@gmail.com>
2022-02-04 23:57:36 +0100

    added some initial variables

--- hf_freeman/battleShip.js
+++ hf_freeman/battleShip.js
@@ -10,3 +10,27 @@ General flow of game:
 4. game finished & user gets rating based on number of guesses
 
 */
+
+// declare and initialize ship location variables
+
+var location1 = Math.round(Math.random() * 4); // initialize randomly later
+var location2 = location1 + 1;
+var location3 = location1 + 2;
+
+// declare user's current guess
+
+var guess;
+
+// declare and initialize number of hits
+
+var hits = 0;
+
+// declare and initialize number of guesses
+
+var guesses = 0;
+
+// declare and initialize if ship is sunk or not
+
+var isSunk = false;
+
+console.log(location1);
