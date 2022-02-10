f0ff1f42ab05551b9b23c30a61cd45b51cdac669
deroj <nickylukic@gmail.com>
2022-02-10 10:02:38 +0100

    first commit

---
+++ small_stuff/woof.js
@@ -0,0 +1,10 @@
+function woof(dogName, dogWeight) {
+  if (dogWeight > 20) {
+    console.log(dogName + " goes WOOF WOOF");
+  } else {
+    console.log(dogName + " goes woof woof");
+  }
+}
+
+woof("Peanut", 6);
+woof("Buster", 46);
