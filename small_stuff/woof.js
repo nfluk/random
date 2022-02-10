dff58b9b4a7a371b39c1d8db7de082e795c283f8
deroj <nickylukic@gmail.com>
2022-02-10 10:43:42 +0100

    added part to capitalize the first letter in name

--- small_stuff/woof.js
+++ small_stuff/woof.js
@@ -3,6 +3,9 @@ function woof(dogName, dogWeight) {
   var dogName = prompt("Type a dog name:", "Dog name");
   var dogWeight = prompt("Type the dogs weight in lbs:", "Dog weight (lbs)");
 
+  // capitalize the first letter in the name parameter
+  dogName = dogName.charAt(0).toUpperCase + dogName.slice(1);
+
   // run the logic to see result
   if (dogWeight > 20) {
     alert(dogName + " goes WOOF WOOF");
