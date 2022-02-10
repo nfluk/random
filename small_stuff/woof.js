420c01c43a93a151963f516d7b3c47084340ad26
deroj <nickylukic@gmail.com>
2022-02-10 10:28:38 +0100

    added comments and removed calling the code inside

--- small_stuff/woof.js
+++ small_stuff/woof.js
@@ -1,11 +1,12 @@
 function woof(dogName, dogWeight) {
+  // get the parameters from user
   var dogName = prompt("Type a dog name:", "Dog name");
   var dogWeight = prompt("Type the dogs weight in lbs:", "Dog weight (lbs)");
+
+  // run the logic to see result
   if (dogWeight > 20) {
     console.log(dogName + " goes WOOF WOOF");
   } else {
     console.log(dogName + " goes woof woof");
   }
 }
-
-woof();
